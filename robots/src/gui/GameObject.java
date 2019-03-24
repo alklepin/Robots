19d64f5a9e220fbd7fa59270b74428c863d91f85
D-rection <43296255+D-rection@users.noreply.github.com>
2019-03-24 16:30:01 +0500

    Add abstract GameObject

---
+++ robots/src/gui/GameObject.java
@@ -0,0 +1,19 @@
+package gui;
+
+import java.awt.*;
+
+public abstract class GameObject {
+    public Point Position;
+    public boolean Alive;
+    public Image Picture;
+
+    public void draw()
+    {
+
+    }
+
+    public void move()
+    {
+
+    }
+}
