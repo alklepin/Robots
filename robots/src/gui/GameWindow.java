5a879da9c02af25869885c7e74e67120c639948a
Vee21 <55953389+VeeNg21@users.noreply.github.com>
2021-05-12 22:37:46 +0500

    Update GameWindow.java

--- robots/src/gui/GameWindow.java
+++ robots/src/gui/GameWindow.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.beans.PropertyVetoException;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
@@ -16,5 +17,30 @@ public class GameWindow extends JInternalFrame
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
+    }
+    
+    public String getPosition()
+    {
+		String position = String.format( "%d,%d,%d,%d,%b", getX(), getY(), 
+									getWidth(), getHeight(), isIcon() );
+		return position;
+    }
+    
+    public void restorePosition(String position)
+    {
+    	// set coordinates and size
+    	String pos[] = position.split(",");
+		setBounds(Integer.parseInt(pos[0]), Integer.parseInt(pos[1]),
+					Integer.parseInt(pos[2]), Integer.parseInt(pos[3]));
+		
+		 // minimize if necessary
+		if (Boolean.parseBoolean(pos[4]))
+		{
+			try {
+				setIcon(true);
+			} catch (PropertyVetoException e) {
+				e.printStackTrace();
+			}
+		}
     }
 }
