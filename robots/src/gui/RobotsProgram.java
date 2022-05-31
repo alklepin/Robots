6841727a156e8def8e39a78cede47605f1f4dd5c
kat17112002 <katakamaeva@gmail.com>
2022-05-31 18:23:50 +0500

    Задание 2

--- robots/src/gui/RobotsProgram.java
+++ robots/src/gui/RobotsProgram.java
@@ -1,9 +1,10 @@
 package gui;
 
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
+import javax.swing.*;
 
 public class RobotsProgram
 {
@@ -19,6 +20,20 @@ public class RobotsProgram
       SwingUtilities.invokeLater(() -> {
         MainApplicationFrame frame = new MainApplicationFrame();
         frame.pack();
+        // 2. Добавить обработку события выхода из приложения
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+          @Override
+          public void windowClosing(WindowEvent e) {
+             if (JOptionPane.showConfirmDialog(frame,
+                    "Выйти из приложения?",
+                    "Подтверждение выхода",
+                    JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+              e.getWindow().dispose();
+              System.exit(0);
+            }
+          }
+        });
         frame.setVisible(true);
         frame.setExtendedState(Frame.MAXIMIZED_BOTH);
       });
