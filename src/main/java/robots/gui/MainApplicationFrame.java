4bdbbe9598c8a5e5120f80c1218f1e375fc263ae
WoodieDudy <goshagks@yandex.ru>
2023-03-26 20:19:40 +0500

    feat: save windows position

--- src/main/java/robots/gui/MainApplicationFrame.java
+++ src/main/java/robots/gui/MainApplicationFrame.java
@@ -3,6 +3,16 @@ package robots.gui;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
@@ -18,13 +28,15 @@ import robots.log.Logger;
 
 /**
  * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается. 
+ * 1. Метод создания меню перегружен функционалом и трудно читается.
  * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- *
  */
-public class MainApplicationFrame extends JFrame
-{
+public class MainApplicationFrame extends JFrame {
     private final JDesktopPane desktopPane = new JDesktopPane();
+
+    private final GameWindow gameWindow;
+
+    private final LogWindow logWindow;
 
     public MainApplicationFrame() {
         //Make the big window be indented 50 pixels from each edge
@@ -32,27 +44,91 @@ public class MainApplicationFrame extends JFrame
         int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-                screenSize.width  - inset*2,
-                screenSize.height - inset*2);
+                screenSize.width - inset * 2,
+                screenSize.height - inset * 2);
 
         setContentPane(desktopPane);
 
 
-        LogWindow logWindow = createLogWindow();
+        logWindow = createLogWindow();
         addWindow(logWindow);
 
-        GameWindow gameWindow = new GameWindow();
-        gameWindow.setSize(400,  400);
+
+        gameWindow = new GameWindow();
+        gameWindow.setSize(400, 400);
         addWindow(gameWindow);
 
+        applyConfig();
+
+        saveConfiguration();
         setJMenuBar(generateMenuBar());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent evt) {
+                saveConfiguration();
+                System.exit(0);
+            }
+        });
     }
 
-    protected LogWindow createLogWindow()
-    {
+    protected void saveConfiguration() {
+        JSONObject json = new JSONObject();
+        json.put("gameWindowX", gameWindow.getX());
+        json.put("gameWindowY", gameWindow.getY());
+        json.put("gameWindowWidth", gameWindow.getWidth());
+        json.put("gameWindowHeight", gameWindow.getHeight());
+
+        json.put("logWindowX", logWindow.getX());
+        json.put("logWindowY", logWindow.getY());
+        json.put("logWindowWidth", logWindow.getWidth());
+        json.put("logWindowHeight", logWindow.getHeight());
+
+        // Save JSON to user home directory
+        String userHomeDir = System.getProperty("user.home");
+        File robotsDir = new File(userHomeDir + "/.Robots");
+        if (!robotsDir.exists()) {
+            robotsDir.mkdir();
+        }
+        File configFile = new File(userHomeDir + "/.Robots/config.json");
+        try (FileWriter fileWriter = new FileWriter(configFile.getAbsolutePath())) {
+            fileWriter.write(json.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void applyConfig() {
+        JSONObject config = readFromConfig();
+        if (config == null) {
+            return;
+        }
+        gameWindow.setSize(((Long) config.get("gameWindowWidth")).intValue(), ((Long) config.get("gameWindowHeight")).intValue());
+        logWindow.setSize(((Long) config.get("logWindowWidth")).intValue(), ((Long) config.get("logWindowHeight")).intValue());
+        gameWindow.setLocation(((Long) config.get("gameWindowX")).intValue(), ((Long) config.get("gameWindowY")).intValue());
+        logWindow.setLocation(((Long) config.get("logWindowX")).intValue(), ((Long) config.get("logWindowY")).intValue());
+
+    }
+
+    protected JSONObject readFromConfig() {
+
+        File configFile = new File(System.getProperty("user.home") + "/.Robots/config.json");
+        JSONParser parser = new JSONParser();
+        if (configFile.exists()) {
+            try {
+                return (JSONObject) parser.parse(new FileReader(configFile));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
+    }
+
+
+    protected LogWindow createLogWindow() {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
-        logWindow.setLocation(10,10);
+        logWindow.setLocation(10, 10);
         logWindow.setSize(300, 800);
         setMinimumSize(logWindow.getSize());
         logWindow.pack();
@@ -60,8 +136,7 @@ public class MainApplicationFrame extends JFrame
         return logWindow;
     }
 
-    protected void addWindow(JInternalFrame frame)
-    {
+    protected void addWindow(JInternalFrame frame) {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
@@ -95,8 +170,7 @@ public class MainApplicationFrame extends JFrame
 //        return menuBar;
 //    }
 
-    private JMenuBar generateMenuBar()
-    {
+    private JMenuBar generateMenuBar() {
         JMenuBar menuBar = new JMenuBar();
 
         JMenu lookAndFeelMenu = new JMenu("Режим отображения");
@@ -124,8 +198,7 @@ public class MainApplicationFrame extends JFrame
 
         JMenu testMenu = new JMenu("Тесты");
         testMenu.setMnemonic(KeyEvent.VK_T);
-        testMenu.getAccessibleContext().setAccessibleDescription(
-                "Тестовые команды");
+        testMenu.getAccessibleContext().setAccessibleDescription("Тестовые команды");
 
         {
             JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
@@ -140,16 +213,12 @@ public class MainApplicationFrame extends JFrame
         return menuBar;
     }
 
-    private void setLookAndFeel(String className)
-    {
-        try
-        {
+    private void setLookAndFeel(String className) {
+        try {
             UIManager.setLookAndFeel(className);
             SwingUtilities.updateComponentTreeUI(this);
-        }
-        catch (ClassNotFoundException | InstantiationException
-               | IllegalAccessException | UnsupportedLookAndFeelException e)
-        {
+        } catch (ClassNotFoundException | InstantiationException
+                 | IllegalAccessException | UnsupportedLookAndFeelException e) {
             // just ignore
         }
     }
