2f61431336be2c7006eb0f5b79986f57af58b8b1
Margarita <margaritayarmolchuk@gmail.com>
2023-04-10 09:22:03 +0500

    fix: game window set size

--- src/main/java/ru/kemichi/robots/gui/GameWindow.java
+++ src/main/java/ru/kemichi/robots/gui/GameWindow.java
@@ -12,11 +12,11 @@ public class GameWindow extends JInternalFrame
     public GameWindow(ResourceBundle bundle, int width, int height)
     {
         super(bundle.getString("gameWindowHeader"), true, true, true, true);
-        super.setSize(width, height);
         m_visualizer = new GameVisualizer();
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
+        super.setSize(width, height);
     }
 }
