d9b73dd3979345305bbf887f2fa6bc62caf68e3d
Margarita Yarmolchuk <margaritayarmolchuk@gmail.com>
2023-04-22 19:20:36 +0500

    fix: GameWindow without size

--- src/main/java/ru/kemichi/robots/gui/MainApplicationFrame.java
+++ src/main/java/ru/kemichi/robots/gui/MainApplicationFrame.java
@@ -18,32 +18,23 @@ import java.util.ResourceBundle;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import ru.kemichi.robots.gui.windows.GameWindow;
+import ru.kemichi.robots.gui.windows.LogWindow;
 import ru.kemichi.robots.log.Logger;
-
-/**
- * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается.
- * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- */
 
 public class MainApplicationFrame extends JFrame {
     private final JDesktopPane desktopPane = new JDesktopPane();
     private final ResourceBundle bundle;
-    private final GameWindow gameWindow;
-    private final LogWindow logWindow;
 
     public MainApplicationFrame(ResourceBundle defaultBundle, int inset) {
-        //Make the big window be indented 50 pixels from each edge
-        //of the screen.
-
         bundle = defaultBundle;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset, screenSize.width - inset * 2, screenSize.height - inset * 2);
 
         setContentPane(desktopPane);
 
-        gameWindow = createGamewindow();
-        logWindow = createLogWindow();
+        GameWindow gameWindow = createGameWindow();
+        LogWindow logWindow = createLogWindow();
 
         addWindow(gameWindow);
         addWindow(logWindow);
@@ -55,17 +46,18 @@ public class MainApplicationFrame extends JFrame {
     protected LogWindow createLogWindow() {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource(), bundle);
         logWindow.setLocation(10, 10);
-        logWindow.setSize(300, 800);
         setMinimumSize(logWindow.getSize());
         logWindow.pack();
+        logWindow.setSize(300, 800);
         Logger.debug(bundle.getString("protocolOK"));
         return logWindow;
     }
 
-    protected GameWindow createGamewindow() {
-        GameWindow gameWindow = new GameWindow(bundle, 1200, 800);
-        gameWindow.setLocation(300, 10);
+    protected GameWindow createGameWindow() {
+        GameWindow gameWindow = new GameWindow(bundle);
+        gameWindow.setLocation(350, 10);
         gameWindow.pack();
+        gameWindow.setSize(800, 800);
         return gameWindow;
     }
 
