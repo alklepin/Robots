fa261a33e2c38c8a9c0f7402e4bf15ee9943b317
GuldenaKuantaeva <91087385+GuldenaKuantaeva@users.noreply.github.com>
2023-04-29 21:34:46 +0600

    Update MainApplicationFrame.java

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -1,175 +1,169 @@
 package gui;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
-import java.awt.*;
-import java.awt.event.WindowEvent;
-
-import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.ParseException;
 
 import javax.swing.*;
-import javax.swing.JDesktopPane;
-import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.event.MenuEvent;
-import javax.swing.JInternalFrame;	
 import javax.swing.event.MenuListener;
-
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import log.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 /**
  * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается. 
+ * 1. Метод создания меню перегружен функционалом и трудно читается.
  * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- *
  */
-public class MainApplicationFrame extends JFrame
-{
+public class MainApplicationFrame extends JFrame {
+
     private final JDesktopPane desktopPane = new JDesktopPane();
-    private final GameWindow gameWindow;
-    private final LogWindow logWindow;
-    
+    RobotModel robotModel = new RobotModel();
+    RobotView robotView = new RobotView();
+    RobotController controller = new RobotController(robotModel, robotView);
+    private final GameWindow gameWindow = new GameWindow(controller);
+    private final LogWindow logWindow = createLogWindow();
+
+    RobotCoordinates coordinatesWindow = new RobotCoordinates();
     public MainApplicationFrame() {
-        //Make the big window be indented 50 pixels from each edge
-        //of the screen.
-        int inset = 50;        
+//Make the big window be indented 50 pixels from each edge
+//of the screen.
+        int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-            screenSize.width  - inset*2,
-            screenSize.height - inset*2);
+                screenSize.width - inset * 2,
+                screenSize.height - inset * 2);
 
         setContentPane(desktopPane);
-        
-        
-        LogWindow logWindow = createLogWindow();
-        addWindow(logWindow);
 
-        GameWindow gameWindow = new GameWindow();
-        gameWindow.setSize(400,  400);
-        addWindow(gameWindow);
-        
+        addWindow(logWindow, 200, 200);
+        addWindow(gameWindow, 400, 400);
+        addWindow(coordinatesWindow, 300, 300);
+        robotModel.addObserver(coordinatesWindow);
         applyConfig();
-        saveConfiguration();
 
+        saveConfiguration();
         setJMenuBar(generateMenuBar());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-                addWindowListener(new java.awt.event.WindowAdapter() {
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
 
             @Override
             public void windowClosing(WindowEvent e) {
-                            saveConfiguration();
-                            exitProgram();
+                saveConfiguration();
+                exitProgram();
             }
         });
-            
+
     }
-    
-    
-    protected LogWindow createLogWindow()
-    {
+
+    protected LogWindow createLogWindow() {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
-        logWindow.setLocation(10,10);
+        logWindow.setLocation(10, 10);
         logWindow.setSize(300, 800);
         setMinimumSize(logWindow.getSize());
         logWindow.pack();
         Logger.debug("Протокол работает");
         return logWindow;
     }
-    
-    protected void addWindow(JInternalFrame frame)
-    {
+
+    protected void addWindow(JInternalFrame frame, int height, int width) {
+        frame.setSize(width, height);
         desktopPane.add(frame);
         frame.setVisible(true);
     }
-    
-//    protected JMenuBar createMenuBar() {
-//        JMenuBar menuBar = new JMenuBar();
-// 
-//        //Set up the lone menu.
-//        JMenu menu = new JMenu("Document");
-//        menu.setMnemonic(KeyEvent.VK_D);
-//        menuBar.add(menu);
-// 
-//        //Set up the first menu item.
-//        JMenuItem menuItem = new JMenuItem("New");
-//        menuItem.setMnemonic(KeyEvent.VK_N);
-//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-//                KeyEvent.VK_N, ActionEvent.ALT_MASK));
-//        menuItem.setActionCommand("new");
-////        menuItem.addActionListener(this);
-//        menu.add(menuItem);
-// 
-//        //Set up the second menu item.
-//        menuItem = new JMenuItem("Quit");
-//        menuItem.setMnemonic(KeyEvent.VK_Q);
-//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-//                KeyEvent.VK_Q, ActionEvent.ALT_MASK));
-//        menuItem.setActionCommand("quit");
-////        menuItem.addActionListener(this);
-//        menu.add(menuItem);
-// 
-//        return menuBar;
-//    }
-    
-    private JMenuBar generateMenuBar()
-    {
+
+// protected JMenuBar createMenuBar() {
+// JMenuBar menuBar = new JMenuBar();
+//
+// //Set up the lone menu.
+// JMenu menu = new JMenu("Document");
+// menu.setMnemonic(KeyEvent.VK_D);
+// menuBar.add(menu);
+//
+// //Set up the first menu item.
+// JMenuItem menuItem = new JMenuItem("New");
+// menuItem.setMnemonic(KeyEvent.VK_N);
+// menuItem.setAccelerator(KeyStroke.getKeyStroke(
+// KeyEvent.VK_N, ActionEvent.ALT_MASK));
+// menuItem.setActionCommand("new");
+//// menuItem.addActionListener(this);
+// menu.add(menuItem);
+//
+// //Set up the second menu item.
+// menuItem = new JMenuItem("Quit");
+// menuItem.setMnemonic(KeyEvent.VK_Q);
+// menuItem.setAccelerator(KeyStroke.getKeyStroke(
+// KeyEvent.VK_Q, ActionEvent.ALT_MASK));
+// menuItem.setActionCommand("quit");
+//// menuItem.addActionListener(this);
+// menu.add(menuItem);
+//
+// return menuBar;
+// }
+
+    private JMenuBar generateMenuBar() {
         JMenuBar menuBar = new JMenuBar();
-        
+
         menuBar.add(createLookAndFeelMenu());
         menuBar.add(createTestMenu());
         menuBar.add(createExitMenu());
-        menuBar.add(exitMenu);
-        return menuBar;	  
-        }
-        
+
+        return menuBar;
+    }
+
+
+    private JMenu createLookAndFeelMenu() {
         JMenu lookAndFeelMenu = new JMenu("Режим отображения");
         lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
-        lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
-                "Управление режимом отображения приложения");
-        
-        {
-            JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
-            systemLookAndFeel.addActionListener((event) -> {
-                setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                this.invalidate();
-            });
-            lookAndFeelMenu.add(systemLookAndFeel);
-        }
+        lookAndFeelMenu.getAccessibleContext().
 
-        {
-            JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
-            crossplatformLookAndFeel.addActionListener((event) -> {
-                setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-                this.invalidate();
-            });
-            lookAndFeelMenu.add(crossplatformLookAndFeel);
-        }
+                setAccessibleDescription(
+                        "Управление режимом отображения приложения");
 
+
+        lookAndFeelMenu.add(
+                createJMenuItem("Системная схема",
+                        KeyEvent.VK_S,
+                        UIManager.getSystemLookAndFeelClassName()));
+
+
+        lookAndFeelMenu.add(createJMenuItem("Универсальная схема",
+                KeyEvent.VK_S,
+                UIManager.getCrossPlatformLookAndFeelClassName()));
+
+        return lookAndFeelMenu;
+
+    }
+
+    private JMenuItem createJMenuItem(String name, int key, String action) {
+        JMenuItem item = new JMenuItem(name, key);
+        item.addActionListener((event) -> {
+            setLookAndFeel(action);
+            this.invalidate();
+        });
+        return item;
+    }
+
+    private JMenu createTestMenu() {
         JMenu testMenu = new JMenu("Тесты");
         testMenu.setMnemonic(KeyEvent.VK_T);
         testMenu.getAccessibleContext().setAccessibleDescription(
                 "Тестовые команды");
-        
+
         {
             JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
             addLogMessageItem.addActionListener((event) -> {
                 Logger.debug("Новая строка");
             });
-                      testMenu.add(addLogMessageItem);
+            testMenu.add(addLogMessageItem);
         }
         return testMenu;
     }
@@ -216,7 +210,6 @@ public class MainApplicationFrame extends JFrame
     {UIManager.put("OptionPane.yesButtonText","Да");
         UIManager.put("OptionPane.noButtonText","Нет");}
 
-
     protected void saveConfiguration() {
         JSONObject json = new JSONObject();
         json.put("gameWindowX", gameWindow.getX());
@@ -228,8 +221,13 @@ public class MainApplicationFrame extends JFrame
         json.put("logWindowY", logWindow.getY());
         json.put("logWindowWidth", logWindow.getWidth());
         json.put("logWindowHeight", logWindow.getHeight());
-        
-                String userHomeDir = System.getProperty("user.home");
+
+        json.put("RobotCoordinatesX", coordinatesWindow.getX());
+        json.put("RobotCoordinatesY", coordinatesWindow.getY());
+        json.put("RobotCoordinatesWidth", coordinatesWindow.getWidth());
+        json.put("RobotCoordinatesHeight", coordinatesWindow.getHeight());
+// Save JSON to user home directory
+        String userHomeDir = System.getProperty("user.home");
         File robotsDir = new File(userHomeDir + "/.Robots");
         if (!robotsDir.exists()) {
             robotsDir.mkdir();
@@ -249,8 +247,10 @@ public class MainApplicationFrame extends JFrame
         }
         gameWindow.setSize(((Long) config.get("gameWindowWidth")).intValue(), ((Long) config.get("gameWindowHeight")).intValue());
         logWindow.setSize(((Long) config.get("logWindowWidth")).intValue(), ((Long) config.get("logWindowHeight")).intValue());
+        coordinatesWindow.setSize(((Long) config.get("RobotCoordinatesWidth")).intValue(), ((Long) config.get("RobotCoordinatesHeight")).intValue());
         gameWindow.setLocation(((Long) config.get("gameWindowX")).intValue(), ((Long) config.get("gameWindowY")).intValue());
         logWindow.setLocation(((Long) config.get("logWindowX")).intValue(), ((Long) config.get("logWindowY")).intValue());
+        coordinatesWindow.setLocation(((Long) config.get("RobotCoordinatesX")).intValue(), ((Long) config.get("RobotCoordinatesY")).intValue());
     }
 
     protected JSONObject readFromConfig() {
@@ -269,18 +269,13 @@ public class MainApplicationFrame extends JFrame
         return null;
     }
 
-    
-    private void setLookAndFeel(String className)
-    {
-        try
-        {
+    private void setLookAndFeel(String className) {
+        try {
             UIManager.setLookAndFeel(className);
             SwingUtilities.updateComponentTreeUI(this);
-        }
-        catch (ClassNotFoundException | InstantiationException
-            | IllegalAccessException | UnsupportedLookAndFeelException e)
-        {
-            // just ignore
+        } catch (ClassNotFoundException | InstantiationException
+                 | IllegalAccessException | UnsupportedLookAndFeelException e) {
+// just ignore
         }
     }
 }
