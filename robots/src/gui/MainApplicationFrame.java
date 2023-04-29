b68e6afece2aa07bd4fc9a6de7ea74940cc0d552
GuldenaKuantaeva <91087385+GuldenaKuantaeva@users.noreply.github.com>
2023-04-29 21:15:57 +0600

    Update MainApplicationFrame.java

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -6,6 +6,10 @@ import java.awt.event.KeyEvent;
 import java.awt.*;
 import java.awt.event.WindowEvent;
 
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
 import javax.swing.*;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
@@ -20,6 +24,11 @@ import javax.swing.event.MenuEvent;
 import javax.swing.JInternalFrame;	
 import javax.swing.event.MenuListener;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
 import log.Logger;
 
 /**
@@ -31,6 +40,8 @@ import log.Logger;
 public class MainApplicationFrame extends JFrame
 {
     private final JDesktopPane desktopPane = new JDesktopPane();
+    private final GameWindow gameWindow;
+    private final LogWindow logWindow;
     
     public MainApplicationFrame() {
         //Make the big window be indented 50 pixels from each edge
@@ -50,6 +61,9 @@ public class MainApplicationFrame extends JFrame
         GameWindow gameWindow = new GameWindow();
         gameWindow.setSize(400,  400);
         addWindow(gameWindow);
+        
+        applyConfig();
+        saveConfiguration();
 
         setJMenuBar(generateMenuBar());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -57,7 +71,8 @@ public class MainApplicationFrame extends JFrame
 
             @Override
             public void windowClosing(WindowEvent e) {
-                exitProgram();
+                            saveConfiguration();
+                            exitProgram();
             }
         });
             
@@ -200,6 +215,60 @@ public class MainApplicationFrame extends JFrame
     //локализация переменных
     {UIManager.put("OptionPane.yesButtonText","Да");
         UIManager.put("OptionPane.noButtonText","Нет");}
+
+
+    protected void saveConfiguration() {
+        JSONObject json = new JSONObject();
+        json.put("gameWindowX", gameWindow.getX());
+        json.put("gameWindowY", gameWindow.getY());
+        json.put("gameWindowWidth", gameWindow.getWidth());
+        json.put("gameWindowHeight", gameWindow.getHeight());
+
+        json.put("logWindowX", logWindow.getX());
+        json.put("logWindowY", logWindow.getY());
+        json.put("logWindowWidth", logWindow.getWidth());
+        json.put("logWindowHeight", logWindow.getHeight());
+        
+                String userHomeDir = System.getProperty("user.home");
+        File robotsDir = new File(userHomeDir + "/.Robots");
+        if (!robotsDir.exists()) {
+            robotsDir.mkdir();
+        }
+        File configFile = new File(userHomeDir + "/.Robots/config.json");
+        try (FileWriter fileWriter = new FileWriter(configFile.getAbsolutePath())) {
+            fileWriter.write(json.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void applyConfig() {
+        JSONObject config = readFromConfig();
+        if (config == null) {
+            return;
+        }
+        gameWindow.setSize(((Long) config.get("gameWindowWidth")).intValue(), ((Long) config.get("gameWindowHeight")).intValue());
+        logWindow.setSize(((Long) config.get("logWindowWidth")).intValue(), ((Long) config.get("logWindowHeight")).intValue());
+        gameWindow.setLocation(((Long) config.get("gameWindowX")).intValue(), ((Long) config.get("gameWindowY")).intValue());
+        logWindow.setLocation(((Long) config.get("logWindowX")).intValue(), ((Long) config.get("logWindowY")).intValue());
+    }
+
+    protected JSONObject readFromConfig() {
+
+        File configFile = new File(System.getProperty("user.home") + "/.Robots/config.json");
+        JSONParser parser = new JSONParser();
+        if (configFile.exists()) {
+            try {
+                return (JSONObject) parser.parse(new FileReader(configFile));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
+    }
+
     
     private void setLookAndFeel(String className)
     {
