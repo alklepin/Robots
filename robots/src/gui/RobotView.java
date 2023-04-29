eedf0920b23860d100345d7ccce5203632d007bc
GuldenaKuantaeva <91087385+GuldenaKuantaeva@users.noreply.github.com>
2023-04-29 21:36:09 +0600

    Add files via upload

---
+++ robots/src/gui/RobotView.java
@@ -0,0 +1,26 @@
+package gui;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+
+
+import static gui.GameVisualizer.*;
+
+public class RobotView
+{
+    public void draw(Graphics2D g, RobotModel robotModel)
+    {
+        int robotCenterX = round(robotModel.getPositionX());
+        int robotCenterY = round(robotModel.getPositionY());
+        AffineTransform t = AffineTransform.getRotateInstance(robotModel.getDirection(), robotCenterX, robotCenterY);
+        g.setTransform(t);
+        g.setColor(Color.MAGENTA);
+        fillOval(g, robotCenterX, robotCenterY, 30, 10);
+        g.setColor(Color.BLACK);
+        drawOval(g, robotCenterX, robotCenterY, 30, 10);
+        g.setColor(Color.WHITE);
+        fillOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+        g.setColor(Color.BLACK);
+        drawOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+    }
+}
